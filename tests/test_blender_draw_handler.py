c5361322a5d1200994f4b1c9274e721c78f1692f
cmdr2 <secondary.cmdr2@gmail.com>
2026-02-27 20:56:49 +0530

    Exit the draw test after it has received enough frames

--- tests/test_blender_draw_handler.py
+++ tests/test_blender_draw_handler.py
@@ -3,10 +3,16 @@
 
 import bpy
 
+frames = 0
+
 
 def test_xr_with_draw_handler():
     def draw_callback():
-        print("Draw handler executed in XR session")
+        global frames
+        frames += 1
+        if frames > 10:
+            print("Test passed: Draw handler executed in XR session")
+            exit(0)
 
     handler = bpy.types.SpaceView3D.draw_handler_add(draw_callback, (), "XR", "POST_VIEW")
 
